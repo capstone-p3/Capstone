4a7a8906774d98a7cfd261a14b0daf21734364b9
TapasSantoki <162741987+TapasSantoki@users.noreply.github.com>
2024-04-04 00:40:31 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <list> // for linked list STL
 #include <vector> // for creating vector of stucture
+#include <cstdlib> // for random function
 
 using namespace std;
 
@@ -15,6 +16,22 @@ struct EntryGateQueue
 int estimateWaitTime(int queueLength, int p)
 {
     return queueLength * p;
+}
+
+int random_gate(int N) {
+  static int gates[N]; 
+  for (int i = 0; i < N; ++i) {
+    gates[i] = i + 1; 
+  }
+
+  int numAttendees = M / 2;
+  for (int i = 0; i < numAttendees; ++i) {
+    int j = rand() % (i + 1); // generate random numbers between 0 and i
+    if (j < i) {
+      swap(gates[i], gates[j]); 
+    }
+  }
+  return gates[numAttendees - 1]; 
 }
 
 int main()
